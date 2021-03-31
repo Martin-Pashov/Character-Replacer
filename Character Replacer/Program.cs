faf8852850f860215d8395c3e349f3f579554321
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2021-03-31 21:13:52 +0300

    Add project files.

---
+++ Character Replacer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Character_Replacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder stringB = new StringBuilder();
+            char s = input[0];
+
+            stringB.Append(s);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (s != input[i])
+                {
+                    s = input[i];
+                    stringB.Append(s);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"The converted word is this: {stringB}");
+        }
+    }
+}
